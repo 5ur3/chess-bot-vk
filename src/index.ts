60cb6bc31c38feb803c17970e183537079184b72
5ur3 <an30.work@gmail.com>
2020-04-01 02:57:58 +0400

    Heroku's https get fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import API from './vk'
 import Sessions from './sessions'
 import http from 'http'
+import https from 'https'
 
 // Heroku support
 http.createServer((req, res) => { res.write(`Heroku please don't kill me I'm a web application!`); res.end(); }).listen(process.env.PORT || 3000);
-setInterval(() => { http.get('https://chess-bot-vk.herokuapp.com/') }, 10 * 60 * 1000)
+setInterval(() => { https.get('https://chess-bot-vk.herokuapp.com/') }, 10 * 60 * 1000)
 
 const sessions = new Sessions()
 const api = new API(sessions);
